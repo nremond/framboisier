12ccaebd844d7e21cbd9ead01b7b56e16d8143de
Nicolas Rémond <nremond+github@gmail.com>
2013-04-09 15:59:29 +0200

    move the sensor part in a dedicated folder

---
+++ sensor/like.c
@@ -0,0 +1,185 @@
+/**
+ * Copyright 2013 Nicolas Rémond (@nremond)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "signal.h"
+#include "string.h"
+#include "assert.h"
+#include "unistd.h"
+
+#include "nfc/nfc.h"
+#include "nfc/nfc-types.h"
+
+#include "led.h"
+#include "nfc-utils.h"
+#include "rest-client.h"
+
+#define MAX_DEVICE_COUNT 16
+
+static nfc_device *pnd = NULL;
+
+void to_hex_string(const uint8_t *buff, size_t buff_size, char *str, size_t str_size)
+{
+  assert(buff != NULL);
+  assert(str != NULL);
+  assert( buff_size*2 < str_size);
+
+  const char * hex = "0123456789abcdef";
+  const uint8_t * pbuff = buff;
+  char * pout = str;
+  for(int j = 0; j < buff_size; ++j){
+    *pout++ = hex[(*pbuff>>4) & 0xF];
+    *pout++ = hex[(*pbuff++) & 0xF];
+  }
+  *pout = 0;
+}
+
+void initialize_ressources()
+{
+  /* NFC Client */
+  nfc_init(NULL);
+  
+  /* REST client */
+  rest_client_initialize();
+
+  /* The control LED should be turned off */
+  turn_led_off();
+}
+
+void cleanup_ressources()
+{
+  nfc_close(pnd);
+  nfc_exit(NULL);
+
+  rest_client_cleanup();
+}
+
+void stop_polling(int sig)
+{
+  (void) sig;
+  if (pnd)
+    nfc_abort_command(pnd);
+  else
+    exit(EXIT_FAILURE);
+}
+
+int nfc_poll(nfc_target *pnt) 
+{
+  const uint8_t uiPollNr = 20;
+  const uint8_t uiPeriod = 2;
+  const nfc_modulation nmModulations[5] = {
+    { .nmt = NMT_ISO14443A, .nbr = NBR_106 },
+    { .nmt = NMT_ISO14443B, .nbr = NBR_106 },
+    { .nmt = NMT_FELICA, .nbr = NBR_212 },
+    { .nmt = NMT_FELICA, .nbr = NBR_424 },
+    { .nmt = NMT_JEWEL, .nbr = NBR_106 },
+  };
+  const size_t szModulations = 5;
+  printf ("NFC device will poll during %ld ms (%u pollings of %lu ms for %zd modulations)\n", (unsigned long) uiPollNr * szModulations * uiPeriod * 150, uiPollNr, (unsigned long) uiPeriod * 150, szModulations);
+  int res = 0;
+  if ((res = nfc_initiator_poll_target (pnd, nmModulations, szModulations, uiPollNr, uiPeriod, pnt))  < 0) {
+    // TODO That should be a "for(;;)" loop !!
+    nfc_perror (pnd, "nfc_initiator_poll_target");
+    cleanup_ressources();
+    exit (EXIT_FAILURE);
+  }
+
+  return res;
+}
+
+void event_loop(const char *server_host)
+{
+  for(;;) {
+    nfc_target nt;
+    const int res = nfc_poll(&nt);
+   
+    if (res > 0) { 
+      // TODO remove that afterwards
+      // bool verbose = false;
+      // print_nfc_target ( nt, verbose );
+
+
+      // To finish ... 
+      if(nt.nm.nmt == NMT_ISO14443A)
+      {        
+        const nfc_iso14443a_info nai = nt.nti.nai;
+        char hex_uid[32];
+        to_hex_string(nai.abtUid, nai.szUidLen, hex_uid, sizeof hex_uid);
+
+        printf("nico_uid=%s\n", hex_uid);
+        post_like_event(server_host, hex_uid);
+
+        turn_led_on();
+        sleep(2); /* in seconds */
+        turn_led_off();
+
+      }
+
+    } else {
+      printf ("No target found.\n");
+    }
+  }
+}
+
+const char* parse_arguments(const int argc, const char *argv[])
+{
+  const char *host_arg = "--server_host";
+  if(argc!=3 || strcmp(argv[1], host_arg)!=0) 
+  {
+    printf("usage: %s %s foo.local\n", argv[0], host_arg);
+    return NULL;
+  } 
+  else 
+  {
+    return argv[2];
+  }
+}
+
+
+int main(const int argc, const char *argv[])
+{
+  const char *server_host = parse_arguments(argc, argv);
+  if(server_host == NULL) {
+    exit(EXIT_FAILURE);
+  }
+
+  signal(SIGINT, stop_polling);
+ 
+  initialize_ressources();
+
+  pnd = nfc_open (NULL, NULL);
+
+  if(pnd == NULL) {
+    ERR ("%s", "Unable to open NFC device.");
+    exit (EXIT_FAILURE);
+  }
+
+  if(nfc_initiator_init (pnd) < 0) {
+    nfc_perror (pnd, "nfc_initiator_init");
+    exit (EXIT_FAILURE);    
+  }
+
+  printf("Events will be sent to 'http://%s' and using libnfc %s\n", server_host, nfc_version());
+  printf("NFC reader: %s opened\n", nfc_device_get_name (pnd));
+ 
+  event_loop(server_host);
+
+  cleanup_ressources();
+  exit (EXIT_SUCCESS);
+}
+
+
+
+
---
+++ sensor/rest-client.c
@@ -0,0 +1,67 @@
+/**
+ * Copyright 2013 Nicolas Rémond (@nremond)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "assert.h"
+#include "stdio.h"
+#include "string.h"
+#include "curl/curl.h"
+
+void rest_client_initialize()
+{
+  curl_global_init(CURL_GLOBAL_ALL);
+}
+
+void rest_client_cleanup()
+{
+  curl_global_cleanup();
+}
+
+size_t http_write(void* buf, size_t size, size_t nmemb, void* userp)
+{
+  printf("body=%s\n", (char*)buf);
+  return nmemb;
+}
+
+const char* post_like_event(const char *server_host, const char *hex_uid)
+{
+  const int post_fields_size = 128;
+  char post_fields[post_fields_size];
+  const char *uid_param_name = "nfcId";
+  assert( strlen(hex_uid) + strlen(uid_param_name) < sizeof(post_fields)+1 );
+  snprintf(post_fields, post_fields_size, "%s=%s", uid_param_name, hex_uid);
+
+  CURL *curl = curl_easy_init();
+  if(curl) {
+    char url[256];
+    snprintf(url, 256, "http://%s/dump/post_form", server_host);
+
+    curl_easy_setopt(curl, CURLOPT_URL, url);
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, post_fields);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, http_write);
+
+    CURLcode res = curl_easy_perform(curl);
+    
+    curl_easy_cleanup(curl);
+
+    if(res != CURLE_OK) {
+      return curl_easy_strerror(res);
+    } else {
+      return NULL;
+    }
+  } else {
+    return "Impossible to initialize CURL";
+  }
+}
