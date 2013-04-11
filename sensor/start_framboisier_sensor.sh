1bb7a5ba664d2e228017ad282aac660ca840658a
Nicolas Rémond <nremond+github@gmail.com>
2013-04-11 16:51:28 +0200

    only root can start the framboisier sensor

--- sensor/start_framboisier_sensor.sh
+++ sensor/start_framboisier_sensor.sh
@@ -1,4 +1,10 @@
-#!/bin/sh
+#!/bin/bash
+
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
 
 echo 27 > /sys/class/gpio/export
 echo out > /sys/class/gpio/gpio27/direction
